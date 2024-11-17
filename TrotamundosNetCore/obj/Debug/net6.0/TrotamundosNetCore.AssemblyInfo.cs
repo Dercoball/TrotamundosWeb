63c63d024f99510b6df11599bd8ce41a10f951d7
AndresDev <you@example.com>
2024-11-16 18:36:18 -0600

    dockerfile

--- TrotamundosNetCore/obj/Debug/net6.0/TrotamundosNetCore.AssemblyInfo.cs
+++ TrotamundosNetCore/obj/Debug/net6.0/TrotamundosNetCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrotamundosNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b03c04de2a74242aefb5d69c00ff1b1cd96556")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrotamundosNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrotamundosNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
